d02318c91d0e43e26dbae8d7793a8b650b6f8855
PRATHA BAGTHARIA <124965525+Pratha3@users.noreply.github.com>
2024-08-28 20:14:41 +0530

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import Folder from "./components/Folder";
-import explorer from "./data/folderData";
-import ComponentFunction from "./components/ComponentFunction";
-import ComponentClass from "./components/ComponentClass";
+// import explorer from "./data/folderData";
+// import ComponentFunction from "./components/ComponentFunction";
+// import ComponentClass from "./components/ComponentClass";
 
-function App() {
-  const [explorerData, setexplorer] = useState(explorer);
+const Counter = () => {
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem("counter");
+    return savedCount !== null ? parseInt(savedCount, 10) : 0; //Base 10 is the standard numbering system we use in everyday life (decimal system). By specifying 10, we tell parseInt to interpret the string as a base-10 number.
+  });
+
+  useEffect(() => {
+    localStorage.setItem("counter", count);
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count > 0 ? count - 1 : 0); // To prevent negative count
+  };
+  // const [explorerData, setexplorer] = useState(explorer);
 
   return (
-    <>
-      {/* <Folder explorer={explorerData} /> */}
-      <ComponentFunction />
-      <ComponentClass />
-    </>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
-}
+};
 
-export default App;
+export default Counter;
